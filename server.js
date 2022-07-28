6b65db9af62a3ea6debf7d491740a9ac0ebb6bdb
tomasagustinsalto@gmail.com <tomasagustinsalto@gmail.com>
2022-07-27 22:12:43 -0300

    chat update

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const {Server: HttpServer} = require("http")
+const {Server: IOServer} = require("socket.io")
+
+const app = express()
+const httpServer = new HttpServer(app);
+const io = new IOServer(httpServer);
+
+httpServer.listen(8080, () =>{
+    console.log("Server on port 8080")
+})
+
+const messages = [
+    { author: "Juan", text: "¡Hola! ¿Que tal?" },
+    { author: "Pedro", text: "¡Muy bien! ¿Y vos?" },
+    { author: "Ana", text: "¡Genial!" }
+ ];
+
+io.on('connection', function(socket) {
+    console.log('Un cliente se ha conectado');
+    socket.emit('messages', messages);
+
+    socket.on("new-message", (data) =>{
+        messages.push(data);
+        io.sockets.emit("messages", messages)
+    })
+});
+
+app.use(express.static("public"));
